95e95b4c0ca8036355045ca7e2e542f8bc6bf196
JamesYanik <=>
2025-06-24 13:46:23 +0200

    better naming

--- module1/models/warriors.py
+++ module1/models/warriors.py
@@ -1,10 +1,10 @@
-class Warrior:
+class Character:
     def __init__(self, hp, atk, spd):
         self.hp = hp
         self.atk = atk
         self.spd = spd
 
 
-class Fighter(Warrior):
+class Fighter(Character):
     def __init__(self):
         super().__init__(100, 10, 20)
