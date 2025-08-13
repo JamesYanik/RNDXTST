a839feae894320c61375f66881186347ef6bb1ee
JamesYanik <=>
2025-08-13 11:47:17 +0200

    just don't

--- module1/controllers/movement.py
+++ module1/controllers/movement.py
@@ -8,6 +8,3 @@ def move_up(character: Character):
 def move_down(character: Character):
     print(character, "moving down")
 
-
-def move_right(character: Character):
-    print(character, "moving right")
